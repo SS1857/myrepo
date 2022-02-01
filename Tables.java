10f54682deafaf5c124e7c6f122e63a111f63e61
SS1857 <89942518+SS1857@users.noreply.github.com>
2022-02-01 20:41:17 +0530

    Add files via upload
    Pushing the tables programs

---
+++ Tables.java
@@ -0,0 +1,20 @@
+package session2;
+
+public class Tables {
+
+    public static void main(String[] args) {
+
+        System.out.println("3 X 1=" + (3*1));
+        System.out.println("3 X 2=" + (3*2));
+        System.out.println("3 X 3=" + (3*3));
+        System.out.println("3 X 4=" + (3*4));
+        System.out.println("3 X 5=" + (3*5));
+
+
+
+
+
+
+
+    }
+}
